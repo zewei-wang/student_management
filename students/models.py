c1813a111bc97e308b309f8b9acf07d626f7029a
Rocker <rocker@xxx.com>
2024-09-28 10:19:49 -0400

    added student model

---
+++ students/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+    enrollment_date = models.DateField()
+    grade = models.IntegerField()
